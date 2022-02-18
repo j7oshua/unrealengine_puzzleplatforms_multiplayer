38d54bbb8edc49db5909d331de40a38b24249b30
Joshua Grimard <jgrimard22@gmail.com>
2022-02-17 19:22:43 -0600

    Added ServerTravel to host the server and ClientTravel for joining the server. --Joshua

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -17,10 +17,20 @@ void UPuzzlePlatformsGameInstance::Host()
 {
 	if (!ensure(GEngine != nullptr)) return;
 	GEngine->AddOnScreenDebugMessage(0, 2.f, FColor::Green, TEXT("Hosting!"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString& IpAddress)
 {
 	if (!ensure(GEngine != nullptr)) return;
 	GEngine->AddOnScreenDebugMessage(0, 2.f, FColor::Green, FString::Printf(TEXT("The IP address is %s"), *IpAddress));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(IpAddress, ETravelType::TRAVEL_Absolute);
 }
